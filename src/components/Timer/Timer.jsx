00b87895239716cfd4cd7b1132caaabd63c3f3a0
Aileen Rae <aileenlrae@gmail.com>
2020-03-28 20:38:15 +0000

    EOD: removing timer SVG for now

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -6,14 +6,10 @@ const Timer = () => {
   return (
     <div className="Timer">
       <div className="Timer-tomato"></div>
-      <svg className="Timer-inner" viewBox="0 0 250 250">
-        {/* TODO: change id to ref */}
-        <path className="Timer-inner-loader" id="loader" transform="translate(125, 125)" />
-      </svg>
-      {/* <div className="Timer-inner">
-      <div id="time-left" className="Timer-countdown">25:00</div>
-      <div id="timer-label" className="Timer-label">focus</div>
-    </div> */}
+      <div className="Timer-inner">
+        <div id="time-left" className="Timer-countdown">25:00</div>
+        <div id="timer-label" className="Timer-label">focus</div>
+      </div>
     </div>
   );
 };
