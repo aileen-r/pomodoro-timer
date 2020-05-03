080aa0174e51e90031136cb9324570299c57a7f9
Aileen Rae <aileen@stormideas.com>
2020-05-03 17:06:59 +0100

    Hook up length increment and decrement buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Timer from './components/Timer';
 import LengthControl from './components/LengthControl';
 import SessionControl from './components/SessionControl';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <main className="App-container">
-      <h1>Pomodoro Timer</h1>
-      <div className="length-controls">
-        <LengthControl initialVal={25} type="focus" />
-        <LengthControl initialVal={5} type="break" />
-      </div>
-      <Timer />
-      <div className="session-controls">
-        <SessionControl id="reset" type="reset"/>
-        <SessionControl id="start_stop" main type="start-stop"/>
-        <SessionControl type="skip"/>
-      </div>
-    </main>
-  </div>
-);
+const App = () => {
+  const [ breakLength, setBreakLength ] = useState(5);
+  const [ focusLength, setFocusLength ] = useState(25);
+  return (
+    <div className="App">
+      <main className="App-container">
+        <h1>Pomodoro Timer</h1>
+        <div className="length-controls">
+          <LengthControl length={focusLength} setLength={x => setFocusLength(x)} type="focus" />
+          <LengthControl length={breakLength} setLength={x => setBreakLength(x)} type="break" />
+        </div>
+        <Timer />
+        <div className="session-controls">
+          <SessionControl id="reset" type="reset" />
+          <SessionControl id="start_stop" main type="start-stop" />
+          <SessionControl type="skip" />
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/components/LengthControl/LengthControl.jsx
+++ src/components/LengthControl/LengthControl.jsx
@@ -1,34 +1,59 @@
 import React from 'react';
 import './LengthControl.css';
 
-const IncrDecrBtn = ({ action, controlLabel, type }) => {
+const IncrDecrBtn = ({ action, controlLabel, onClick, type }) => {
   let icon;
   switch (action) {
-    case 'increment':
+    case 'Increment':
       icon = '+';
       break;
-    case 'decrement':
+    case 'Decrement':
       icon = '-';
       break;
     default:
       break;
   }
-  const btnId = (action && type) ? type + '-' + action : '';
-  return (action && <button id={btnId} aria-label={action + ' ' + controlLabel}>{icon}</button>) || null;
+  const btnId = (action && type ? type + '-' + action : '').toLowerCase();
+  return (
+    (action && (
+      <button id={btnId} aria-label={action + ' ' + controlLabel} onClick={onClick}>
+        {icon}
+      </button>
+    )) ||
+    null
+  );
 };
 
-const LengthControl = ({ initialVal, type }) => {
+const LengthControl = ({ length, setLength, type }) => {
   const controlLabel = type && type[0].toUpperCase() + type.slice(1) + ' Length';
   const typeId = type && type === 'focus' ? 'session' : type;
   const controlLabelId = typeId && typeId + '-label';
   const valueId = typeId && typeId + '-length';
 
+  const increment = () => {
+    setLength(length + 1);
+  };
+
+  const decrement = () => {
+    setLength(length -1);
+  };
+
   return (
     <div className="LengthControl">
       <p id={controlLabelId}>{controlLabel}</p>
-      <IncrDecrBtn action="decrement" controlLabel={controlLabel} type={typeId}/>
-      <span id={valueId}>{initialVal}</span>
-      <IncrDecrBtn action="increment" controlLabel={controlLabel} type={typeId}/>
+      <IncrDecrBtn
+        action="Decrement"
+        controlLabel={controlLabel}
+        onClick={decrement}
+        type={typeId}
+      />
+      <span id={valueId}>{length}</span>
+      <IncrDecrBtn
+        action="Increment"
+        controlLabel={controlLabel}
+        onClick={increment}
+        type={typeId}
+      />
     </div>
   );
 };
